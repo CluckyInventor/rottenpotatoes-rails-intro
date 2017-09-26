5a175319ebfddc6202795d60562acb731efed5d0
Simon Schneider <cluckyinventor@gmail.com>
2017-09-26 03:56:44 +0000

    fixed the checkboxes not appearing checked in part 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,23 +11,30 @@ class MoviesController < ApplicationController
   end
 
   def index
+    #it may not be the best solution, but I thought to feed the array given to the checkboxes both the name and if it is chosen.
+    #rating_choices is what I assign that to. Rating_selections is just the array of checked ratings for the filtering code.
     unless params[:ratings]
-      @rating_choices = ["G", "PG", "PG-13", "R"]
+      @rating_choices = [{name:"G", chosen: true}, {name: "PG", chosen: true}, {name: "PG-13", chosen: true},{name: "R", chosen: true}]
+      rating_selections = ["G", "PG", "PG-13", "R"]
     else
-      @rating_choices = params[:ratings].keys
+      rating_selections = params[:ratings].keys
+      @rating_choices = [{name:"G", chosen: false}, {name: "PG", chosen: false}, {name: "PG-13", chosen: false},{name: "R", chosen: false}]
+      rating_selections.each do |rating|
+        @rating_choices[@rating_choices.index() {|item| item[:name] == rating }] = {name: rating, chosen: true}
+      end
     end
     #Not sure why the following line isn't working, so I'm just gonna hard code it :(
     # @all_ratings = Movie.all_ratings
-    @all_ratings = ["G","PG","PG-13","R"]
+    # @all_ratings = ["G","PG","PG-13","R"]
     #Using unless is probably backwards here, but I felt like putting the default case first
     unless params[:sorting] == "Date"
       #@movies = Movie.all.sort {|mov, other| mov.title <=> other.title}
-      @movies = Movie.where({rating: @rating_choices}).reorder('title')
+      @movies = Movie.where({rating: rating_selections}).reorder('title')
       @titleClass = "hilite"
       @dateClass = ""
     else
       #@movies = Movie.all.sort {|mov, other| mov.release_date <=> other.release_date}
-      @movies = Movie.where({rating: @rating_choices}).reorder('release_date')
+      @movies = Movie.where({rating: rating_selections}).reorder('release_date')
       @titleClass = ""
       @dateClass = "hilite"
     end
